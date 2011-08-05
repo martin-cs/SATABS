42f1fd9d9e7a7da7c4b95985e39212417ea3f33f
kroening <kroening@c82bf971-7879-4458-aa57-88e1f0fec4b8>
2011-08-05 15:48:02 +0000

    fix
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/satabs/trunk@3 c82bf971-7879-4458-aa57-88e1f0fec4b8

--- regression/safety/Pointer12/main.c
+++ regression/safety/Pointer12/main.c
@@ -1,6 +1,6 @@
-typedef unsigned __int8  uint8_t;
-typedef unsigned __int16 uint16_t;
-typedef unsigned __int32 uint32_t;
+typedef unsigned char    uint8_t;
+typedef unsigned short   uint16_t;
+typedef unsigned int     uint32_t;
 
 #ifndef TOSH_DATA_LENGTH
 #define TOSH_DATA_LENGTH 28
