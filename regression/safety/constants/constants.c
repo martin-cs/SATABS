a6a9971759d8464535cf4eb80503d371887f9347
ally <ally@c82bf971-7879-4458-aa57-88e1f0fec4b8>
2011-08-09 20:15:04 +0000

    Added test
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/satabs/trunk@29 c82bf971-7879-4458-aa57-88e1f0fec4b8

---
+++ regression/safety/constants/constants.c
@@ -0,0 +1,36 @@
+
+
+int foo(int w)
+{
+	
+	
+	int x;
+	
+	int y;
+	
+	int z;
+	
+	if(w)
+	{
+		x = x;
+	}
+			
+		x = 2;
+	
+		y = x;
+	
+		z = y;
+		
+	
+	
+	if(x == 2)
+	{
+		x = x;
+	}
+	
+	x = z;
+	
+	assert(z == 3);
+
+
+}
