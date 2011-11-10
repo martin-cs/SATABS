5f48927fbf8f002472e08e121cc059e6289e9705
tautschnig <tautschnig@c82bf971-7879-4458-aa57-88e1f0fec4b8>
2011-11-10 14:42:34 +0000

    test.pl: test whether directory has test.desc before attempting to run test
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/satabs/trunk@111 c82bf971-7879-4458-aa57-88e1f0fec4b8

--- regression/test.pl
+++ regression/test.pl
@@ -124,7 +124,7 @@ sub dirs() {
   my @list;
 
   opendir CWD, ".";
-  @list = grep { !/^\./ && -d "$_" && !/CVS/ } readdir CWD;
+  @list = grep { !/^\./ && -d "$_" && !/CVS/ && -s "$_/test.desc" } readdir CWD;
   closedir CWD;
 
   @list = sort @list;
