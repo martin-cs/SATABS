203507640c03f521ce1ae6e9da203307b432ca5f
kroening <kroening@c82bf971-7879-4458-aa57-88e1f0fec4b8>
2013-01-04 16:41:39 +0000

    boppo regression test file format
    git-svn-id: svn+ssh://svn.cprover.org/var/lib/cvs/satabs/trunk@288 c82bf971-7879-4458-aa57-88e1f0fec4b8

--- regression/test.pl
+++ regression/test.pl
@@ -54,7 +54,7 @@ sub test($$$$$) {
   $options =~ s/$ign//g if(defined($ign));
 
   my $output = $input;
-  $output =~ s/\.(c|i|cpp|xml)$/.out/;
+  $output =~ s/\.(c|i|cpp|xml|bp)$/.out/;
 
   if($output eq $input) {
     print("Error in test file -- $test\n");
